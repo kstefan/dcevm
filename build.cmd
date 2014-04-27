2d38672449399a4e5d112372179aee16d892af6a
Karel Stefan <karel.stefan@etnetera.cz>
2014-04-27 19:48:31 +0200

    Modifications for my environment Add eclipse files Change cygwin path

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@ set WINDOWS_SDK=c:\Program Files\Microsoft SDKs\Windows\v7.1
 call "%WINDOWS_SDK%\bin\setenv.cmd" /%ARCH%
 
 echo Script arguments: %*
-set CYGWIN=C:\cygwin
+set CYGWIN=C:\dev\cygwin
 set SCRIPT=%~dp0
 set HOTSPOTWORKSPACE=hotspot
 %CYGWIN%\bin\bash --login -c 'cd `/usr/bin/cygpath "$SCRIPT"/hotspot` ; /usr/bin/pwd ; /usr/bin/make -C "make" %*'
